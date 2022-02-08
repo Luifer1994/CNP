edb8db0f9cac2ef0f204fb6ddfa3791d529555be
luifer Almendrales <almendralesluifer@gmail.com>
2022-02-08 17:19:35 -0500

    config santum

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
@@ -52,21 +53,20 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
-        $rules = array(
-            'email' => 'required|email',
-            'password' => 'required',
-        );
-        $validator = Validator::make($request->all(), $rules);
-        if ($validator->fails()) {
-            return $validator->errors();
-        }
-        $user = User::whereEmail($request->email)->first();
+        $request->validate([
+            'email'       => 'required|email',
+            'password'    => 'required',
+        ]);
 
+        // Obtenemos al usuario a autenticar
+        $user = User::where('email', $request->email)->first();
+
+        // Vemos si las credenciales son erróneas para retornar un mensaje de error
         if ($user && Hash::check($request->password, $user->password)) {
             $token = $user->createToken('Laravel')->accessToken;
             return response()->json([
                 'res' => true,
-                'token' => $token,
+                'token' => $user->createToken('CNP')->plainTextToken,
                 'message' => 'Bienvenido al sistema',
             ], 200);
         } else {
@@ -75,5 +75,19 @@ class UserController extends Controller
                 'message' => 'Email o password incorrecto',
             ], 400);
         }
+    }
+
+    public function logout()
+    {
+        //Obtenemos usuario logeado
+        $user = Auth::user();
+        //Busca todos los token del usuario en la base de datos y los eliminamos;
+        $user->tokens->each(function ($token) {
+            $token->delete();
+        });
+        return response()->json([
+            'res' => true,
+            'message' => 'Hasta la vista Baby',
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,14 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-/* Route::group(['middleware' => 'auth:api'], function () {
+//Rutas protegidas
+Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::controller(UserController::class)->group(function () {
         Route::get('/users-list', 'list');
         Route::post('/user-store', 'store');
+        Route::get('/user-logout', 'logout');
     });
-}); */
+});
 
 //Login
 Route::post('/user-login', [UserController::class, 'login']);
