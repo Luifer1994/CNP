a8b99cb90eadf20c714e0811b98287341b3b9d98
luifer Almendrales <almendralesluifer@gmail.com>
2022-02-09 09:20:06 -0500

    config seeders products and center op

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        //User
         \App\Models\User::factory(1)->create();
+
+        //Products
+        $json = '[
+            {
+                "name": "JABON BAR AZULK*250GR  ACC SUAV",
+                "cod" : 4904,
+                "ref": "7702310014168"
+            },
+            {
+                "name": "CAFE COLCAFE *50GR GRANULADO",
+                "cod" : 533,
+                "ref": "7702032253258"
+            }
+            ]';
+        $products = json_decode($json);
+        foreach ($products as $product) {
+
+            DB::table('products')->insert([
+                "cod" => $product->cod,
+                "name" => $product->name,
+                "id_reference" => $product->ref,
+                "created_at" => now(),
+                "updated_at" => now(),
+            ]);
+        }
+
+        //Center OP
+        DB::table('center_operations')->insert([
+            "cod" => 101,
+            "name" => "PRADO",
+            "created_at" => now(),
+            "updated_at" => now(),
+        ]);
     }
 }
